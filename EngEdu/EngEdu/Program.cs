d01779db718089fedd5d4dc97acadfc3f4ba728f
Wannamiu <58030218@kmitl.ac.th>
2017-10-19 17:07:31 +0700

    3

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -13,8 +13,7 @@ namespace EngEdu
             Console.WriteLine("Year 2560 semester 1, I am EngEdu Freshman");
             Console.WriteLine("Year 2560 semester 2, I learnd to program in C shrap.");
             Console.WriteLine("Year 2561 semester 1, I have project project project !!!!!.");
-            Console.WriteLine("Year 2561 semester 2, I have project project project !!!!!.");
-
+            Console.WriteLine("Year 2561 semester 2, I have project project project1 !!!!!.");
         }
     }
 }
